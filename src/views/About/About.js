f15aac351ff5eebe4367d6aceb9d7257cedcaf25
Catherine Pedersen <cathy.pedersen@gmail.com>
2021-04-24 16:22:52 -0400

    Update About help

--- src/views/About/About.js
+++ src/views/About/About.js
@@ -17,15 +17,15 @@ const About = (props) => {
           The My Spotify Data application provides a way for users to search for
           songs in their playlists, functionality not provided by Spotify's user
           interface. In addition, My Spotify Data displays the user's listening
-          history and creates a CSV file containing the user's playlists and
-          songs. In this way, the user can always be assured that they have a
-          backup of their Spotify data.
+          history and can be used to create a CSV file containing the user's
+          playlists and songs. In this way, the user can always be assured that
+          they have a backup of their Spotify data.
         </p>
         <p>
           By logging into My Spotify Data, you agree to provide your music data
           to My Spotify Data, so that you can search and export your data. Your
           data is not accessible to other users and will not be sold to third
-          parties.
+          parties. Your password is not saved to the My Spotify Data database.
         </p>
       </div>
     </section>
